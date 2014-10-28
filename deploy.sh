5762c5bb1d32a96fdc7502e69417373fce8dbb94
Kees Kwekkeboom <kees@kees-PC.(none)>
2014-10-28 15:09:52 +0100

    Changed fstab of flasher

--- deploy.sh
+++ deploy.sh
@@ -20,19 +20,21 @@ create_flasher_image()
 	echo "Copying ${FLASHER_IMAGE} to $ROOTFS"
 	sudo rm -r ${ROOTFS}/*
 	sudo tar -xf ${BUILD_DIR}/flasher-image-vaf-beaglebone.tar.xz -C ${ROOTFS}
+	sudo cp sources/meta-angstrom/recipes-core/base-files/base-files/fstab ${ROOTFS}/etc
 
-	echo "Copying .dtb files and kernel"
+	echo "Copying kernel"
 	sudo cp ~/Documents/projects/pem2/images/BBB_flasher_image/boot/uImage_vaf ${ROOTFS}/boot
 	sudo ln -sf /boot/uImage_vaf ${ROOTFS}/boot/uImage
 
-	echo "Copying image to flash"
-		sudo mkdir ${BOOT}/linux
-		sudo cp ${BUILD_DIR}/console-image-vaf-beaglebone.tar.xz ${BOOT}/linux
+	echo "Copying deployment image"
+	sudo mkdir -p ${BOOT}/linux
+	sudo cp ${BUILD_DIR}/console-image-vaf-beaglebone.tar.xz ${BOOT}/linux
 
-	echo "Coyping bootloader MLO and uEnvt"
+	echo "Coyping bootloader MLO and uEnv"
 	sudo cp ~/Documents/projects/pem2/images/BBB_flasher_image/boot/MLO ${BOOT}
 	sudo cp ~/Documents/projects/pem2/images/BBB_flasher_image/boot/u-boot.img ${BOOT}
 	sudo cp ~/Documents/projects/pem2/images/BBB_flasher_image/boot/uEnv.txt ${BOOT}
+	sync
 }
 
 prepare_sdcard()
@@ -42,20 +44,30 @@ prepare_sdcard()
 	read -p "Press [Enter] to continue..."
 	umount ${1}1
 	umount ${1}2
-	{ 
-		echo o
-		echo n
-		echo p
-		echo 1
-		echo 
-		echo 100000
-		echo n
-		echo p
-		echo 2
-		echo 
-		echo 400000
-		echo w
-	}	| fdisk $1
+	export LC_ALL=C
+
+	if [ $# -ne 1 ]; then
+		echo "Usage: $0 <drive>"
+		exit 1;
+	fi
+
+	DRIVE=$1
+	
+	dd if=/dev/zero of=$DRIVE bs=1024 count=1024
+
+	SIZE=`fdisk -l $DRIVE | grep Disk | grep bytes | awk '{print $5}'`
+
+	echo DISK SIZE - $SIZE bytes
+
+	CYLINDERS=`echo $SIZE/255/63/512 | bc`
+
+	echo CYLINDERS - $CYLINDERS
+	{
+	echo ,5,0x0C,*
+	echo ,21,,-
+	} | sfdisk -D -H 255 -S 63 -C $CYLINDERS $DRIVE
+
+	sleep 1
 	sync
 	mkfs.msdos ${1}1 -n "boot"
 	mkfs.ext2 ${1}2 -L "rootfs"
